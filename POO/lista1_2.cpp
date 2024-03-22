36b4e046c420565fc438950859bdcbeb0f529da4
Luisfelipel <127263172+champola9@users.noreply.github.com>
2024-03-21 23:23:16 -0300

    Update lista1_2.cpp

--- POO/lista1_2.cpp
+++ POO/lista1_2.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Estudante
+{
+private:    
+    string nome;
+    int matricula;
+    string curso;
+public:
+    void cadastrar(string name, int mat, string cur);
+    void exibir();
+};
+
+void Estudante::cadastrar(string name, int mat, string cur)
+{
+    cout<<"Digite o nome do estudante.\n";
+    cin>> name;
+    cout<<"Digite a matrícula do estudante.\n";
+    cin>> mat;
+    cout<<"Digite o curso do estudante.\n";
+    cin>>cur;
+    
+    nome = name;
+    matricula = mat;
+    curso = cur;
+}
+
+void Estudante::exibir()
+{
+    cout<<"-------------------------------------------\n";
+    cout<<"Nome: "<<nome<<"\n";
+    cout<<"Matrícula: "<<matricula<<"\n";
+    cout<<"Curso: "<<curso<<"\n";
+    cout<<"-------------------------------------------\n";
+}
+
+int main()
+{
+    Estudante MyEstudante;
+    MyEstudante.cadastrar("Fulano",000000000,"Place_holder");
+    MyEstudante.exibir();
+}
