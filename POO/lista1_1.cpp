51120c2c30e7941c0b66dcc019d4c64e4555aba9
Luisfelipel <127263172+champola9@users.noreply.github.com>
2024-03-21 23:23:42 -0300

    Update lista1_1.cpp

--- POO/lista1_1.cpp
+++ POO/lista1_1.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class agenda_tel
+{
+private:
+int telefone;
+string nome;
+
+    public:
+    agenda_tel(int telefone, string nome) {
+        this->telefone = telefone;
+        this->nome = nome;
+    }
+    int getTelefone() {
+        return telefone;
+    }
+
+    string getNome() {
+        return nome;
+    }
+
+    void mostrarTelefone() {
+        cout << "O telefone é: " << telefone<<"\n";
+    }
+
+    void mostrarNome() {
+        cout << "O nome é: " << nome<<"\n";
+    }
+};
+
+int main() {
+    agenda_tel Myagenda_tel(40028922, "Luis");
+    Myagenda_tel.mostrarTelefone();
+    Myagenda_tel.mostrarNome();
+
+    return 0;
+}
